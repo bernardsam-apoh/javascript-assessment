6e02ad828c5da60b6064315ef1f52a636bc3955a
bernardsam-apoh <sb.bernardsam@gmail.com>
2023-03-20 10:46:08 +0000

    Changed function name

--- solution1.js
+++ solution1.js
@@ -1,4 +1,4 @@
-const parseText = (text) => {
+const countWords = (text) => {
 	let textArr = text.replace(/[^0-9a-z\s]/gi, '').split(' ')
 	const textMap = textArr.reduce((acc, e) => acc.set(e, (acc.get(e) || 0) + 1), new Map())
 
@@ -15,7 +15,9 @@ const parseText = (text) => {
 	bucket.sort().forEach((element) => {
 		console.log(`"${element.split(':')[0]}":${element.split(':')[1]}`)
 	})
+
+    return bucket
 }
 
 const sentence = 'I am a very very nice man, am I not? What do you think? I think I am!'
-parseText(sentence)
+countWords(sentence)
